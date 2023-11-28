814c51aeb275c83141dccad29b735a5f05114f8a
DanaeXY <121288187+DanaeXY@users.noreply.github.com>
2023-11-28 20:52:50 +0100

    Update Tarea.java

--- GestorDeTareas/Tarea.java
+++ GestorDeTareas/Tarea.java
@@ -1,4 +1,11 @@
 public class Tarea {
-    
-    
+     private String nombre;
+
+    public Tarea(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }    
 }
