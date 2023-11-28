ad03424dc96290ca70e6bd3496eb9185ec8deb3c
DanaeXY <121288187+DanaeXY@users.noreply.github.com>
2023-11-28 21:33:27 +0100

    Update Tarea.java

--- GestorDeTareas/Tarea.java
+++ GestorDeTareas/Tarea.java
@@ -1,12 +1,39 @@
-package GestorDeTareas;
-public class Tarea {
-     private String nombre;
+package Gestor_Tareas;
 
+import Gestor_Tareas.Tarea;
+
+public class Tarea {
+ 
+    private String nombre;
+    private Prioridad prioridad;
+
+    // Constructor sin inicialización de prioridad
     public Tarea(String nombre) {
         this.nombre = nombre;
     }
 
+    // Constructor con inicialización de prioridad
+    public Tarea(String nombre, Prioridad prioridad) {
+        this.nombre = nombre;
+        this.prioridad = prioridad;
+    }
+
     public String getNombre() {
         return nombre;
-    }    
+    }
+
+    public Prioridad getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Prioridad prioridad) {
+        this.prioridad = prioridad;
+    }
+}
+
+
+enum Prioridad {
+    BAJA,
+    MEDIA,
+    ALTA
 }
