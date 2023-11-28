7901810642bbc8c1b69010fd42071f876a95ca3a
Carolina <a23carolinabc@iessanclemente.net>
2023-11-28 12:41:19 +0100

    Proyecto java limpio

--- GestorDeTareas/GestorDeTareas.java
+++ GestorDeTareas/GestorDeTareas.java
@@ -2,23 +2,6 @@ import java.util.ArrayList;
 
 public class GestorDeTareas {
 
-
-    
-public static void main(String[] args) {
-
- ArrayList<Tarea> listaTareas = new ArrayList<Tarea>();
-
- Tarea tarea1 = new Tarea();
- Tarea tarea2 = new Tarea();
- Tarea tarea3 = new Tarea();
-
-    listaTareas.add(tarea1);
-    listaTareas.add(tarea2);
-    listaTareas.add(tarea3);
-
-
-    
-}
 }
 
 
